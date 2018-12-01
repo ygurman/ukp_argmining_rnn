7328b03c47e8fd36bfd1cd064292e5e570a02ed0
ygurman <38371646+ygurman@users.noreply.github.com>
2018-12-02 00:12:07 +0200

    Add files via upload

--- src/models.py
+++ src/models.py
@@ -3,12 +3,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.autograd as autograd
 import torch.optim as optim
 import os
 import numpy as np
 import pickle
 import sys
 
+UNK_TOKEN_SYMBOL = "UNKNOWN_TOKEN"
 torch.manual_seed(361)
 
 
@@ -114,7 +116,7 @@ def prepare_glove_embeddings(glove_embds_path, glove_txt_name):
 
     return words, word2ix, glove_embds
 
-def combine_word_vocab(dataset_voc, pretrained_embs, d_embd):
+def combine_word_vocab(dataset_voc, pretrained_embs, d_embd, save_path=None):
     """
     combine dataset's vocabulary with GloVe to create an embedding layer for words (initiating unkown tokens with random normal distributed weights)
     :param dataset_voc:
@@ -135,25 +137,41 @@ def combine_word_vocab(dataset_voc, pretrained_embs, d_embd):
             new_weights[new_ix] = weight
             new_ix += 1
 
+    # add UNKNOWN_TOKEN_SYMBOL for later use
+    new_word2ix[UNK_TOKEN_SYMBOL] = new_ix
+    new_weights[new_ix] = np.random.normal(scale=0.5,size=(d_embd,)) # TODO - same
+
     # create weight matrix
-        voc_size = len(new_word2ix.keys())
+    voc_size = len(new_word2ix.keys())
     weight_matrix = np.zeros((voc_size,d_embd))
 
     for word, ix in new_word2ix.items():
         try:
-            weight_matrix[i] = pretrained_embs[word]
+            weight_matrix[ix] = pretrained_embs[word]
         except KeyError:
-            weight_matrix[i] = new_weights[i]
+            weight_matrix[ix] = new_weights[ix]
 
     # create embedding layer
     embd_layer = nn.Embedding(num_embeddings=voc_size,embedding_dim=d_embd)
     embd_layer.load_state_dict({'weight':weight_matrix})
 
+    if save_path:
+        with open(save_path+"_word2ix.pcl", "wb") as f:
+            pickle.dump(obj=new_word2ix,file=f)
+        with open(save_path+"_embdLayer.pcl", "wb") as f:
+            pickle.dump(obj=embd_layer,file=f)
+
     return new_word2ix, embd_layer
 
 # actual model
 class BiLSTM_Segmentor_Classifier(nn.Module):
-    def __init__(self, d_word_embd, d_pos_embd, d_h1, n_lstm_layers, ac_tagset_size, len_seq, pretraind_embd_layer=None):
+    """
+    BiLSTM based AC segmentor-Classifier. 1st part of the e2e model
+    """
+    def __init__(self, d_word_embd, d_pos_embd,
+                 d_h1, n_lstm_layers, ac_tagset_size,
+                 len_seq, pretraind_embd_layer=None):
+
         super().__init__()
         self.h1dimension = d_h1
 
@@ -162,39 +180,44 @@ class BiLSTM_Segmentor_Classifier(nn.Module):
         self.emd_pos - nn.Embedding(num_embeddings=len_seq,embedding_dim=d_pos_embd)
 
         # sequence (bilstm) layer - recive previous concatenated embeddings and pass to linear classification (for tagging)
-        self.seq_layer = nn.LSTM(input_size = d_word_embd + d_pos_embd, # input as concatenated word|pos embeddings
+        self.lstm1 = nn.LSTM(input_size = d_word_embd + d_pos_embd, # input as concatenated word|pos embeddings
                                  hidden_size = d_h1,                    # hidden state parameter (for token level AC class) to pass to the final linear layer
                                  num_layers = n_lstm_layers,            # number of LSTM layers (set to 1 but passed as parameter for reusability)
                                  bidirectional = True)                  # sets as bi-drectional LSTM
 
         # initialize first hidden states
-        self.hidden1 = init_hidden(self.h1dimension)
+        self.hidden1 = self.init_hidden(self.h1dimension)
 
         # set hidden linear layer (for classification) that maps hidden states to AC type tags
-        self.hidden2AC_tag = nn.Linear(in_features = d_h1, out_features = ac_tagset_size)
+        self.hidden2AC_tag = nn.Linear(in_features = d_h1 * 2, out_features = ac_tagset_size)
 
     def init_hidden(self, hidden_dim):
+        """
+        initialize hidden states of the biLSTM
+        :param hidden_dim:
+        :return:
+        """
         # initialize hidden states with zeroes (2 because bidirectional lstm)
         h0 = autograd.Variable(torch.zeros(1 * 2, self.batch_size, hidden_dim))
         c0 = autograd.Variable(torch.zeros(1 * 2, self.batch_size, hidden_dim))
         return h0, c0
 
-    # def init_word_embeddings(self.pre_trained_embeddings_path):
-    #     pass # TODO
 
-
-    def forward():
-        ## AC-type recognition side
+    def forward(self, prepared_sequence):
+        """
+        :param prepared_sequence: tuple of 3 indexed lists (created via "prepare sequence method) - indexed tokens and POSs
+        """
+        indexed_tokens, indexed_POSs = prepared_sequence
         # pass word and pos indices as input
-        w_embd = self.embd_word_layer(word)
-        pos_embd = self.embd_pos_layer(pos)
+        w_embd = self.embd_word_layer(indexed_tokens)
+        pos_embd = self.embd_pos_layer(indexed_POSs)
         # concatenate pos and word embeddings
         embd_output = torch.cat((w_embd, pos_embd), -1)
 
         # pass embedding layer output to lstm
-        lstm_output, self.hidden1 = self.lstm1_ent(embd_output.view(self.h1dimension, self.batch_size, -1), self.hidden1)
+        lstm_output, self.hidden1 = self.lstm1(embd_output.view(embd_output.size(0), self.batch_size, -1), self.hidden1)
         # pass lstm output to hidden liner layer
-        tag_space = self.hidden2AC_tag(lstm_output.view(-1,lstm_output.size(-1)))
+        tag_space = self.hidden2AC_tag(lstm_output.view(len(prepared_sequence[0]),-1))
         tag_scores = F.log_softmax(tag_space, dim=-1)
         return tag_scores
 
